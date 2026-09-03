4a8b24732c2cef0612ca0fb9a92ea617ff55d3cd
Suliman <sulimanrageb015@gmail.com>
2026-09-04 00:25:23 +0300

    Exercise 1

--- Assignment-Advanced-03/Program.cs
+++ Assignment-Advanced-03/Program.cs
@@ -1,1 +1,39 @@
 ﻿
+#region Exercise 1: Student Grade Manager
+//List<int> grades = new List<int> { 85, 92, 78, 95, 88, 70, 100, 65 };
+//foreach (var grade in grades)
+//{
+//    Console.WriteLine(grade);
+//}
+
+//Console.WriteLine($"Grades: {grades.Count}");
+//Console.WriteLine($" firsr Grades: {grades.First()}");
+//Console.WriteLine($" last Grades: {grades.Last()}");
+
+//Sort the grades ascending, then print
+//grades.Sort();
+//foreach (var grade in grades)
+//{
+//    Console.WriteLine(grade);
+//}
+////Get the first grade above 90
+//Console.WriteLine(grades.Find(g => g > 90));
+
+//Get all grades below 75 (failing grades)
+//List<int> failingGrades = grades.FindAll(g => g < 75);
+//foreach (var grade in failingGrades)
+//{
+//    Console.WriteLine(grade);
+//}
+////Remove all failing grades (below 75)
+//int failingGrades1 = grades.RemoveAll(g => g <75);
+//Console.WriteLine(failingGrades1); // 2 removed grades
+
+//Console.WriteLine(grades.Contains(100)); // true
+//List<string> Gradestring = new List<string> {"Grade: 85","Grade: 92","Grade: 78","Grade: 95","Grade: 88","Grade: 70","Grade: 100","Grade: 65"};
+//Gradestring.Sort();
+//foreach (var grade in Gradestring)
+//{
+//    Console.WriteLine(grade);
+//} 
+#endregion
